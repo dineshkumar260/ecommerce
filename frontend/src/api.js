11c2b578011a7d9ba81bd516933973494e8bcf13
dineshkumar260 <dineshkumarsara07@gmail.com>
2026-04-18 09:21:13 +0530

    Update API URL to production Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Match backend dev port
+    baseURL: 'https://ecommerce-ssew.onrender.com/api',
 });
 
 api.interceptors.request.use(config => {
